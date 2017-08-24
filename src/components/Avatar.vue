577872790d0ca8884a60abb1aadd34942e8d9812
arsenalup <676250063@qq.com>
2017-08-24 23:54:02 +0800

    8.24

---
+++ src/components/Avatar.vue
@@ -0,0 +1,15 @@
+<template>
+  <span class="v-avatar" :style="styleSize" :aria-label="alt">
+  <span class="v-avatar-text" v-text="name" :style="{background: bg, color: fg}"></span>
+  </span>
+</template>
+<script>
+import wc from "word-color"
+//alt 传入的用户名
+// size 头像尺寸
+export default {
+	props: ['alt', 'size']
+
+	data()
+}
+</script>
