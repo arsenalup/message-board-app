78b4fc14c024593ff49a32351500e7c9e82d070a
arsenalup <676250063@qq.com>
2017-08-28 23:04:01 +0800

    8.28

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+// 添加这行代码后，在 App.vue 及及其他组件中就不用导入 `axios` 了，可以直接用
+// http.get(...), http.post(...) ...
+window.http = axios
 
 new Vue({
   el: '#app',
